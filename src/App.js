e558ccd2f888358401b07247e566fd4f6cb49696
Rafał Kornobis <rafalkornobis@gmail.com>
2023-04-19 11:42:08 +0200

    [RoadMap](first):After html changes

--- src/App.js
+++ src/App.js
@@ -26,91 +26,91 @@ function App() {
         <div className='insideBlue'>
           <div className='logoWrapper'>
             <img src={Up} alt='meetup logo' height='100px' width='170px' />
-            <div> <h1 className='text1'> Attend The Most Awaited Conference of 2015 </h1></div>
-            <div> <h2 className='text2'> to start you up with your business!</h2></div>
-            <div> <p className='text3'> 20 to 22 October, 2015</p></div>
+            <h1 className='text1'> Attend The Most Awaited Conference of 2015 </h1>
+            <h2 className='text2'> to start you up with your business!</h2>
+            <p className='text3'> 20 to 22 October, 2015</p>
             <button className='button' > buy tickets now </button>
           </div>
-          <div>
             <img className='blueWrapIMG' src={rocket} alt='rocket' height='200px' width='200px' />
           </div>
-        </div>
-
       </header>
 
-          <div> <h2 className='text4'> Why This Event Is Super Cool?</h2></div>
+          <h2 className='text4'> Why This Event Is Super Cool?</h2>
       <div style={{alignItems:'center', justifyContent:'center'}} className='rowWrapper'>
+
           <div className='eventImg'>
             <img src={ticket} alt='ticket' height='150px' width='200px' />
-            <div> <h3 className='offers'>Buy Early Bird Tickets </h3></div>
-            <div> <p className='text5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            <h3 className='offers'>Buy Early Bird Tickets </h3>
+            <p className='text5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu bibendum at 
-            varius vel pharetra.</p></div>
+            varius vel pharetra.</p>
           </div>
 
           <div  className='eventImg'>
             <img src={expert} alt='expert' height='150px' width='200px' />
-            <div> <h3 className='offers'>Expert speakers </h3></div>
-            <div> <p className='text5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            <h3 className='offers'>Expert speakers </h3>
+            <p className='text5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu bibendum at
-            varius vel pharetra.</p></div>
+            varius vel pharetra.</p>
           </div>
+
           <div  className='eventImg'>
             <img src={globe} alt='globe' height='150px' width='200px' />
-            <div> <h3 className='offers'>People around the globe </h3></div>
-            <div> <p className='text5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            <h3 className='offers'>People around the globe </h3>
+            <p className='text5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu bibendum at 
-            varius vel pharetra.</p></div>
+            varius vel pharetra.</p>
           </div>
       </div>
 
-          <div> <h2 className='text4'> Meet the Speakers</h2> </div>
+          <h2 className='text4'> Meet the Speakers</h2>
       <div style={{alignItems:'center', justifyContent:'center'}} className='gridContainer'> 
+
           <div className='speaker'>
             <img src={speaker} alt='Speaker1' height='100px' width='100px' />
-            <div> <h3 className='offers'>Simon Colins </h3></div>
-            <div> <p className='text5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></div>
+            <h3 className='offers'>Simon Colins </h3>
+            <p className='text5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             <div className='Icon'><FacebookIcon style={{color:'darkBlue'}}/><TwitterIcon style={{color:'rgb(44, 146, 230)'}}/><LinkedInIcon style={{color:'blue'}}/></div>
           </div>
 
           <div className='speaker2'>
             <img src={speaker2} alt='Speaker1' height='100px' width='100px' />
-            <div> <h3 className='offers'>Simon Colins </h3></div>
-            <div> <p className='text5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></div>
+            <h3 className='offers'>Simon Colins </h3>
+            <p className='text5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             <div className='Icon'><FacebookIcon style={{color:'darkBlue'}}/><TwitterIcon style={{color:'rgb(44, 146, 230)'}}/><LinkedInIcon style={{color:'rgb(31, 101, 230)'}}/></div>
           </div>
 
           <div className='speaker3'>
             <img src={speaker3} alt='Speaker1' height='100px' width='100px' />
-            <div> <h3 className='offers'>Simon Colins </h3></div>
-            <div> <p className='text5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></div>
+            <h3 className='offers'>Simon Colins </h3>
+            <p className='text5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             <div className='Icon'><FacebookIcon style={{color:'darkBlue'}}/><TwitterIcon style={{color:'rgb(44, 146, 230)'}}/><LinkedInIcon style={{color:'rgb(31, 101, 230)'}}/></div>
           </div>
 
           <div className='speaker4'>
             <img src={speaker4} alt='Speaker1' height='100px' width='100px' />
-            <div> <h3 className='offers'>Simon Colins </h3></div>
-            <div> <p className='text5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></div>
+            <h3 className='offers'>Simon Colins </h3>
+            <p className='text5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             <div className='Icon'><FacebookIcon style={{color:'darkBlue'}}/><TwitterIcon style={{color:'rgb(44, 146, 230)'}}/><LinkedInIcon style={{color:'rgb(31, 101, 230)'}}/></div>
           </div>
 
           <div className='speaker5'>
             <img src={speaker5} alt='Speaker1' height='100px' width='100px' />
-            <div> <h3 className='offers'>Simon Colins </h3></div>
-            <div> <p className='text5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></div>
+            <h3 className='offers'>Simon Colins </h3>
+            <p className='text5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             <div className='Icon'><FacebookIcon style={{color:'darkBlue'}}/><TwitterIcon style={{color:'rgb(44, 146, 230)'}}/><LinkedInIcon style={{color:'rgb(31, 101, 230)'}}/></div>
           </div>
 
           <div className='speaker6'>
             <img src={speaker6} alt='Speaker1' height='100px' width='100px' />
-            <div> <h3 className='offers'>Simon Colins </h3></div>
-            <div> <p className='text5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></div>
+            <h3 className='offers'>Simon Colins </h3>
+            <p className='text5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             <div className='Icon'><FacebookIcon style={{color:'darkBlue'}}/><TwitterIcon style={{color:'rgb(44, 146, 230)'}}/><LinkedInIcon style={{color:'rgb(31, 101, 230)'}}/></div>
           </div>
       </div>
@@ -132,58 +132,51 @@ function App() {
         </div>   
         </div>
       </div>
-      <div>
 
-        <div> <h2 className='text4'> Event Schedule</h2></div>
-        <div style={{alignItems:'center', justifyContent:'center'}} className='eventWrapper'>
+      <div>
+        <h2 className='text4'> Event Schedule</h2>
+        <div style={{justifyContent:'center'}} className='eventWrapper'>
+          
           <div className='event1'>
-            <div>
-            <img style={{margin:'10px'}}src={speaker6} alt='Event Speaker' height='70px' width='70px'/>
-            </div>
+              <img style={{margin:'10px'}}src={speaker6} alt='Event Speaker' height='70px' width='70px'/>
             <div>
               <p className='boldTime'> 10am to 11am </p>
               <p className='boldEvent'> the designer's guide to being essential</p>
               <p className='eventText'> Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
               sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             </div>
-            
           </div>
+
           <div className='event2'>
-            <div>
             <img style={{margin:'10px'}}src={speaker4} alt='Event Speaker' height='70px' width='70px'/>
-            </div>
             <div>
               <p className='boldTime'> 10am to 11am </p>
               <p className='boldEvent'> the designer's guide to being essential</p>
               <p className='eventText'> Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
               sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             </div>
-            
           </div>
+
           <div className='event3'>
-            <div>
             <img style={{margin:'10px'}}src={speaker2} alt='Event Speaker' height='70px' width='70px'/>
-            </div>
             <div>
               <p className='boldTime'> 10am to 11am </p>
               <p className='boldEvent'> the designer's guide to being essential</p>
               <p className='eventText'> Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
               sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             </div>
-            
           </div>
+
           <div className='event4'>
-            <div>
             <img style={{margin:'10px'}}src={speaker2} alt='Event Speaker' height='70px' width='70px'/>
-            </div>
             <div>
               <p className='boldTime'> 10am to 11am </p>
               <p className='boldEvent'> the designer's guide to being essential</p>
               <p className='eventText'> Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
               sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             </div>
-            
           </div>
+
         </div>
       </div>
 
@@ -207,13 +200,14 @@ function App() {
                 <div className='sendWrapper'><button className='buttonSend'> SEND -></button></div>
               </div>
             </div>
+
           <div>
             <div  className='contact'> Twitter Feed</div>
             <div className='underLine'><p className='text6'> Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
                   sed do eiusmod tempor</p></div>
             <div className='underLine'><p className='text6'> Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
                   sed do eiusmod tempor</p></div>
-            <div ><p className='text6' > Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            <div><p className='text6' > Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
                   sed do eiusmod tempor</p></div>
           </div>
         </div>
